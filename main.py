594deed6f3a901a84b9f3cc9d26d289f62fa953a
Palash Sureka <pal.palashsureka@gmail.com>
2020-12-17 13:23:49 +0530

    Stuff

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask , render_template
 import pickle
-import src.training
+import src.training as training
 app = Flask('Covid Infection Probability Detector')
 
 picklefile = open('src/data.pickle', 'rb')
@@ -12,7 +12,7 @@ picklefile.close()
 def main():
     inputf = [100, 1, 22, 1, 1]
     infProb = clf.predict_proba([inputf])[0][1]
-    return str(infProb)
+    return render_template('index.html')
 
 if __name__ == "__main__":
     app.run(debug=True,port=5000)
