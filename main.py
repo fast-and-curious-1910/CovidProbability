a684278d78306f978eee2acccc7c82a7ced37a1b
Palash Sureka <pal.palashsureka@gmail.com>
2020-12-18 17:53:51 +0530

    Form handling and some stuff

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask , render_template , redirect , request
+from flask import Flask, render_template, redirect, request
 import pickle
 import os
 import flask
@@ -11,9 +11,12 @@ picklefile = open(picklefilepath, 'rb')
 clf = pickle.load(file=picklefile)
 
 picklefile.close()
-@app.route('/' , methods=['POST','GET'])
+
+
+@app.route('/', methods=['POST', 'GET'])
 def main():
     if request.method == 'POST':
+        
 
         mainDict1 = request.form
         fever = int(mainDict1['fever'])
@@ -22,8 +25,13 @@ def main():
         runnyNose = int(mainDict1['runnyNose'])
         diffBreath = int(mainDict1['diffBreath'])
 
-    inputf = [100, 1, 22, 1, 1]
-    infProb = clf.predict_proba([inputf])[0][1]
+
+        inputf = [fever,pain, age, runnyNose, diffBreath]
+        # inputf = "i like cheese"
+        infProb = clf.predict_proba([inputf])[0][1]
+        return render_template('showProb.html',inf=round(infProb*100))
+
+
     return render_template('index.html')
 
 
@@ -31,10 +39,12 @@ def main():
 def err(error):
     return flask.Response(status=404)
 
+
 @app.route('/github')
 def redirect_to_github():
 
     return redirect('https://github.com/fast-and-curious-1910/CovidProbability', code=302)
+
 
 @app.route('/source')
 def redirect_to_github2():
@@ -43,6 +53,4 @@ def redirect_to_github2():
 
 
 if __name__ == "__main__":
-    app.run(debug=False,port=5000,host='localhost')
-
-
+    app.run(debug=False, port=5000, host='localhost')
